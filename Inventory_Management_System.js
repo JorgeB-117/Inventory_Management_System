ab6629825e33f5e0e91333f80551e1c865a418c3
Jorge Bernal <jorgebernal@usf.edu>
2024-09-15 11:31:02 -0400

    Initialize inventory with product objects

---
+++ Inventory_Management_System.js
@@ -0,0 +1,12 @@
+// Task 1: Create an Inventory Array of Product Objects
+
+let Inventory = [
+    {name: "Laptop", price: 1500, quantity: 20, lowStockLevel: 5},
+    {name: "Phone", price: 1000, quantity: 40, lowStockLevel: 10},
+    {name: "Tablet", price: 1250, quantity: 30, lowStockLevel: 5},
+    {name: "Headphones", price: 300, quantity: 60, lowStockLevel: 15},
+    {name: "Speaker", price: 200, quantity: 70, lowStockLevel: 20},
+];
+
+console.log(Inventory[0].name);
+
